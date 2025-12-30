e74fa3c2417b6966f4efc28a285cfdd2b19de5cb
ge9 <ge9@users.noreply.github.com>
2025-12-30 17:33:57 +0900

    update 1.21.11

--- src/client/java/monkey/lumpy/horse/stats/vanilla/mixin/client/HorseScreenMixin.java
+++ src/client/java/monkey/lumpy/horse/stats/vanilla/mixin/client/HorseScreenMixin.java
@@ -7,6 +7,8 @@ import monkey.lumpy.horse.stats.vanilla.util.Converter;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.gui.screen.ingame.HorseScreen;
+import net.minecraft.client.gui.screen.ingame.MountScreen;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.passive.AbstractDonkeyEntity;
 import net.minecraft.entity.passive.AbstractHorseEntity;
@@ -22,15 +24,13 @@ import java.math.BigDecimal;
 import java.text.DecimalFormat;
 
 @Mixin(HorseScreen.class)
-public abstract class HorseScreenMixin extends HandledScreen<HorseScreenHandler> {
-    @Shadow
-    @Final
-    private AbstractHorseEntity entity;
+public abstract class HorseScreenMixin extends MountScreen<HorseScreenHandler> {
 
     private ModConfig config;
 
-    public HorseScreenMixin(HorseScreenHandler handler, PlayerInventory inventory, Text title) {
-        super(handler, inventory, title);
+    public HorseScreenMixin(HorseScreenHandler handler, PlayerInventory inventory, Text title, int slotColumnCount, LivingEntity mount)
+    {
+        super(handler, inventory, title, slotColumnCount, mount);
     }
 
     protected void drawForeground(DrawContext drawContext, int mouseX, int mouseY) {
@@ -39,11 +39,11 @@ public abstract class HorseScreenMixin extends HandledScreen<HorseScreenHandler>
             config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
         }
         if (config.showValue()) {
-            boolean hasChest = AbstractDonkeyEntity.class.isAssignableFrom(this.entity.getClass()) && ((AbstractDonkeyEntity) this.entity).hasChest();
+            boolean hasChest = AbstractDonkeyEntity.class.isAssignableFrom(this.mount.getClass()) && ((AbstractDonkeyEntity) this.mount).hasChest();
             DecimalFormat df = new DecimalFormat("#.#");
-            String jumpStrength = df.format(Converter.jumpStrengthToJumpHeight(this.entity.getAttributeValue(EntityAttributes.JUMP_STRENGTH)));
-            String maxHealth = df.format(this.entity.getMaxHealth());
-            String speed = df.format(Converter.genericSpeedToBlocPerSec(this.entity.getAttributes().getValue(EntityAttributes.MOVEMENT_SPEED)));
+            String jumpStrength = df.format(Converter.jumpStrengthToJumpHeight(this.mount.getAttributeValue(EntityAttributes.JUMP_STRENGTH)));
+            String maxHealth = df.format(this.mount.getMaxHealth());
+            String speed = df.format(Converter.genericSpeedToBlocPerSec(this.mount.getAttributes().getValue(EntityAttributes.MOVEMENT_SPEED)));
 
             // Coloring
             Color jumpColor = config.getNeutralColor();
@@ -93,8 +93,8 @@ public abstract class HorseScreenMixin extends HandledScreen<HorseScreenHandler>
 
             Color strengthColor = config.getNeutralColor();
 
-            if (LlamaEntity.class.isAssignableFrom(this.entity.getClass())) {
-                int strength = 3 * ((LlamaEntity) this.entity).getStrength();
+            if (LlamaEntity.class.isAssignableFrom(this.mount.getClass())) {
+                int strength = 3 * ((LlamaEntity) this.mount).getStrength();
 
                 if(config.useColors()) {
                     if(strength > config.getGoodStrengthValue()) {strengthColor = config.getGoodColor();}
